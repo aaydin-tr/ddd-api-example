3f61b684893aad25bc860ee624a55fc55aa45e5e
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-11-16 17:05:52 +0300

    refactor: set logger silent

--- infrastructure/db/postgresql/postgresql.go
+++ infrastructure/db/postgresql/postgresql.go
@@ -5,13 +5,16 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewPostgresDB(host, user, password, dbname string, port int) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		host, user, password, dbname, port)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect database: %w", err)
 	}
